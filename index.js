0f37e42e8fc4129b819738ac51ca848acdbd9e55
Zhixing Zhang <me@neoto.xin>
2020-05-31 13:08:26 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const http = require('http');
+const https = require('https');
+const
+{
+	URL
+} = require('url');
+
+const gm = require('gm').subClass(
+{
+	imageMagick: true
+});
+
+const bucketName = 'assets.cytoid.io';
+
+function requestURLForPath(url)
+{
+	let path = url.pathname;
+	if (path.startsWith('/'))
+	{
+		path = path.substr(1)
+	}
+	return `https://www.googleapis.com/storage/v1/b/${bucketName}/o/${encodeURIComponent(path)}?alt=media`;
+}
+
+function requestListener(req, res)
+{
+	if (req.method !== 'GET')
+	{
+		res.writeHead(405);
+		res.end('Method not allowed');
+	}
+	const url = new URL(req.url, 'https://images.cytoid.io');
+	const request = https.get(requestURLForPath(url), (response) =>
+	{
+		if (response.statusCode === 200)
+		{
+			// Image exists.
+			res.writeHead(200);
+
+
+			let height = url.searchParams.get('h');
+			if (height) height = parseInt(height) || null;
+			let width = url.searchParams.get('w');
+			if (width) width = parseInt(width) || null;
+
+
+			if (height && width)
+			{
+				gm(response)
+					.resize(width, height, '^')
+					.gravity('Center')
+					.crop(width, height)
+					.stream()
+					.pipe(res);
+			} else if (height || width) {
+				gm(response)
+					.resize(width, height)
+					.stream()
+					.pipe(res);
+			}
+			else
+			{
+				response.pipe(res);
+			}
+
+		}
+		else
+		{
+			// Error
+			res.writeHead(response.statusCode);
+			response.pipe(res);
+		}
+	});
+}
+
+const port = process.env.PORT || 8040;
+const server = http.createServer(requestListener);
+server.listen(port, '0.0.0.0', () =>
+{
+	console.log('Server listening on ' + port);
+});
